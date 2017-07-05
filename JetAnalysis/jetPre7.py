3319ca2c16374ea667fed9a672dc108edf2a9b4d
br384 <br384@yu.yale.edu@spinup-0005ac.yu.yale.edu>
2017-07-05 16:39:40 -0400

    fixing submax algorithm

--- JetAnalysis/jetPre7.py
+++ JetAnalysis/jetPre7.py
@@ -4,7 +4,7 @@ from itertools import izip
 import ROOT
 import array
 import numpy
-
+import math
 def readTree(filename1, filename2):
     fIn = ROOT.TFile(filename1, "READ")
     tree = fIn.Get("tree")
@@ -37,10 +37,12 @@ def readTree(filename1, filename2):
         for j in range(jEvent.nJets): #j tells you which jet you are in
             print("\tJet number %d" %j)
             print("\tjEvent.pIndex[j][0] = %d" % jEvent.pIndex[j][0])
+            
+            #max placeholder for finding highest-energy particle in a jet
             maxm = jEvent.pIndex[j][0]
-
+            #loop through and find max energy
             for k, index in enumerate(jEvent.pIndex[j]):
-		#index will give the particle index, eg [7, 21, 32], k gives 0, 1, 2
+       #index will give the particle index, eg [7, 21, 32], k gives 0, 1, 2
                 print("\t\t\tenergy[%d] = %f" % (jEvent.pIndex[j][k], pEvent.energy[jEvent.pIndex[j][k]]))
                 #events[0].energy[event[1].pIndex[j][k]] #how to tap into event number on other tree
                 #tree.event.energy[event.pIndex[j][k]] #how to tap into event number on other tree
@@ -52,46 +54,45 @@ def readTree(filename1, filename2):
             print("\t\tmax: %d" % maxm)
 
             if(len(jEvent.pIndex[j])>1):
-		for k, index in enumerate(jEvent.pIndex[j]): #finding the particle in the jet with the second highest energy
-		    if (maxm == jEvent.pIndex[j][0]):
-		        submax = jEvent.pIndex[j][1]
-		    else:
-		        submax = jEvent.pIndex[j][0]
+                if (maxm == jEvent.pIndex[j][0]):
+                   submax = jEvent.pIndex[j][1]
+                else:
+                   submax = jEvent.pIndex[j][0]
+                for k, index in enumerate(jEvent.pIndex[j]): #finding the particle in the jet with the second highest energy
 
-		    if ((pEvent.energy[submax] < pEvent.energy[jEvent.pIndex[j][k]]) and (pEvent.energy[jEvent.pIndex[j][k]] < pEvent.energy[maxm])):
-		        submax = jEvent.pIndex[j][k]
-
+                    if ((pEvent.energy[submax] < pEvent.energy[jEvent.pIndex[j][k]]) and (pEvent.energy[jEvent.pIndex[j][k]] < pEvent.energy[maxm])):
+                        submax = jEvent.pIndex[j][k]
                 print("\t\tsubmax: %d" % submax)
 
             
-            star = numpy.arctan((jEvent.eta[maxm]-jEvent.eta[submax])/(jEvent.phi[maxm]-jEvent.phi[submax]))
+        # star = numpy.arctan((jEvent.eta[maxm]-jEvent.eta[submax])/(jEvent.phi[maxm]-jEvent.phi[submax]))
 
-            #define the jet axis
-            phijet = jetTree.phi[j]
-            etajet = jetTree.y[j]
+        #     #define the jet axis
+        # phijet = jetTree.phi[j]
+        # etajet = jetTree.eta[j]
 
-            for k, index in enumerate(pEvent.pIndex[j]):
-                ptot = math.sqrt(math.pow(jEvent.px[index], 2) + math.pow(pEvent.px[index], 2) + math.pow(jEvent.px[index]))
-                phi = math.acos(jEvent.px[index]/(math.sqrt(math.pow(jEvent.px[index], 2)+math.pow(jEvent.py[index], 2))))
-                eta = jEvent.pz[index]/ptot
+        # for k, index in enumerate(jEvent.pIndex[j]):
+        #     ptot = math.sqrt(math.pow(pEvent.px[index], 2) + math.pow(pEvent.px[index], 2) + math.pow(pEvent.px[index], 2))
+        #     phi = math.acos(pEvent.px[index]/(math.sqrt(math.pow(pEvent.px[index], 2)+math.pow(pEvent.py[index], 2))))
+        #     eta = pEvent.pz[index]/ptot
 
-                #centre the jet axis
-                phi = phi-phijet
-                eta = eta-etajet
+        #         #centre the jet axis
+        #     phi = phi-phijet
+        #     eta = eta-etajet
 
-                #fill histogram for centre
+        #         #fill histogram for centre
 
-                histcentre.Fill(phi, eta)
+        #     histcentre.Fill(phi, eta)
 
-                #rotate
-                           #check which arctan
-                alpha = math.atan(eta/phi) #fill in numbers
-                r = math.sqrt(math.pow(phi, 2) + math.pow(eta, 2))
-                phi = r * math.cos(alpha-star)
-                eta = r * math.sin(alpha-star)
+        #         #rotate
+        #                    #check which arctan
+        #     alpha = math.atan(eta/phi) #fill in numbers
+        #     r = math.sqrt(math.pow(phi, 2) + math.pow(eta, 2))
+        #     phi = r * math.cos(alpha-star)
+        #     eta = r * math.sin(alpha-star)
                 
-                #fill histogram for rotate
-                histrotate.Fill(phi, eta)       
+                # fill histogram for rotate
+            # histrotate.Fill(phi, eta)       
     #see others for examples of iterating through
         
     canvascentre.cd()
