a4fc05273bf009b408e8b33bd5910e551c99fc7c
br384 <br384@yu.yale.edu@spinup-0005ac.yu.yale.edu>
2017-07-07 15:23:12 -0400

    jetpre outputs textfile with jets, but there are many empty jets

--- JetAnalysis/jetPre7.py
+++ JetAnalysis/jetPre7.py
@@ -6,6 +6,10 @@ import array
 import numpy
 import math
 def readTree(filename1, filename2):
+
+    DIMENSION_JET_IMAGE = 3
+    COLLISION_TYPE = "pp"
+
     fIn = ROOT.TFile(filename1, "READ")
     tree = fIn.Get("tree")
     tree.Print()
@@ -14,26 +18,41 @@ def readTree(filename1, filename2):
     jetTree = fIn2.Get("jetTree")
     jetTree.Print()
 
-    histbefore = ROOT.TH2F("histbefore", "histbefore", 20, -2, 2, 20, -2, 2) #bin bound bound bin bound bound
-    histbefore.GetXaxis().SetTitle("phi");
-    histbefore.GetYaxis().SetTitle("eta");
-    histbefore.GetZaxis().SetTitle("idk");
-    histcentre = ROOT.TH2F("histcentre", "histcentre", 20, -2, 2, 20, -2, 2) #bin bound bound bin bound bound
-    histcentre.GetXaxis().SetTitle("phi");
-    histcentre.GetYaxis().SetTitle("eta");
-    histcentre.GetZaxis().SetTitle("idk");
-    histrotate = ROOT.TH2F("histrotate", "histrotate", 20, -2, 2, 20, -2, 2) #bin bound bound bin bound bound
-    histrotate.GetXaxis().SetTitle("phi");
-    histrotate.GetYaxis().SetTitle("eta");
-    histrotate.GetZaxis().SetTitle("idk");
-    histTranslate = ROOT.TH2F("histTranslate", "histTranslate", 50, -1, 1, 50, -1, 1) #bin bound bound bin bound bound
+    histBefore = ROOT.TH2F("histBefore", "histBefore", 20, -2, 2, 20, -2, 2) #bin bound bound bin bound bound
+    histBefore.GetXaxis().SetTitle("phi");
+    histBefore.GetYaxis().SetTitle("eta");
+    histBefore.GetZaxis().SetTitle("counts weighted by energy");
+
+    histCentre = ROOT.TH2F("histCentre", "histCentre", 20, -2, 2, 20, -2, 2) #bin bound bound bin bound bound
+    histCentre.GetXaxis().SetTitle("phi");
+    histCentre.GetYaxis().SetTitle("eta");
+    histCentre.GetZaxis().SetTitle("counts weighted by energy");
+
+    histRotate = ROOT.TH2F("histRotate", "histRotate", 20, -2, 2, 20, -2, 2) #bin bound bound bin bound bound
+    histRotate.GetXaxis().SetTitle("phi");
+    histRotate.GetYaxis().SetTitle("eta");
+    histRotate.GetZaxis().SetTitle("counts weighted by energy");
+
+    histTranslate = ROOT.TH2F("histTranslate", "histTranslate", 20, -1, 1, 20, -1, 1) #bin bound bound bin bound bound
     histTranslate.GetXaxis().SetTitle("phi");
     histTranslate.GetYaxis().SetTitle("eta");
-    histTranslate.GetZaxis().SetTitle("idk");
-    canvasbefore = ROOT.TCanvas("canvasbefore", "canvasbefore")
-    canvascentre = ROOT.TCanvas("canvascentre", "canvascentre")
-    canvasrotate = ROOT.TCanvas("canvasrotate", "canvasrotate")
+    histTranslate.GetZaxis().SetTitle("counts weighted by energy");
+
+    histReflect = ROOT.TH2F("histReflect", "histReflect", 20, -1, 1, 20, -1, 1) #bin bound bound bin bound bound
+    histReflect.GetXaxis().SetTitle("phi");
+    histReflect.GetYaxis().SetTitle("eta");
+    histReflect.GetZaxis().SetTitle("counts weighted by energy");
+
+    histJetTemp = ROOT.TH2F("histJetTemp", "histJetTemp", DIMENSION_JET_IMAGE, -5, 5, DIMENSION_JET_IMAGE, -5, 5) # numBins bound bound bin bound bound
+    histJetTemp.GetXaxis().SetTitle("phi");
+    histJetTemp.GetYaxis().SetTitle("eta");
+    histJetTemp.GetZaxis().SetTitle("counts weighted by energy");
+
+    canvasBefore = ROOT.TCanvas("canvasBefore", "canvasBefore")
+    canvasCentre = ROOT.TCanvas("canvasCentre", "canvasCentre")
+    canvasRotate = ROOT.TCanvas("canvasRotate", "canvasRotate")
     canvasTranslate = ROOT.TCanvas("canvasTranslate", "canvasTranslate")
+    canvasReflect = ROOT.TCanvas("canvasReflect", "canvasReflect")
 
     '''
 
@@ -42,21 +61,31 @@ def readTree(filename1, filename2):
         tuple_list[i] = (a, new_b)
 
     '''
+    open('output.txt', 'w').close()
     iEvent = 0;
+    output = open("output.txt" , "w" )
+
+#LOOP: through each event in tree
     for pEvent, jEvent in  izip(tree, jetTree): #zip
         print("Event %d:" % iEvent)
-        iEvent+=1
     #for event in jetTree:
         print("jEvent.nJets is %d" %jEvent.nJets)
         print("pEvent.nParticles is %d" %pEvent.nFinalParticles)
-        
+#LOOP: through each jet in event    
         for j in range(jEvent.nJets): #j tells you which jet you are in
+             
             print("\tJet number %d" %j)
             print("\tjEvent.pIndex[j][0] = %d" % jEvent.pIndex[j][0])
             
             #max placeholder for finding highest-energy particle in a jet
             #loop through and find max energy
             maxm = jEvent.pIndex[j][0]
+            phiTempV = []
+            etaTempV = []
+            energyTempV = []
+            sumEtaPos = 0
+            sumEtaNeg = 0
+#LOOP: through each particle in jet
             for k, index in enumerate(jEvent.pIndex[j]):
        #index will give the particle index, eg [7, 21, 32], k gives 0, 1, 2
                 print("\t\t\tenergy[%d] = %f" % (jEvent.pIndex[j][k], pEvent.energy[jEvent.pIndex[j][k]]))
@@ -95,7 +124,7 @@ def readTree(filename1, filename2):
                 eta = math.atanh(pEvent.pz[index]/pTot)
 
                 #before anything
-                histbefore.Fill(phi, eta)
+                histBefore.Fill(phi, eta, pEvent.energy[index])
 
         #center the jet axis
                 phijet = jEvent.phi[j]
@@ -112,7 +141,7 @@ def readTree(filename1, filename2):
 
 
                 #fill histogram for centre
-                histcentre.Fill(phi, eta)
+                histCentre.Fill(phi, eta, pEvent.energy[index])
             #########
             #rotate
                 if(len(jEvent.pIndex[j])>1):
@@ -138,44 +167,73 @@ def readTree(filename1, filename2):
 
 
                 # fill histogram for rotate
-                    histrotate.Fill(phi, eta)
+                    histRotate.Fill(phi, eta)
                     #next step of translation
                     phi = phi - phi_maxm
                     eta = eta - eta_maxm
 
 
-                    histTranslate.Fill(phi,eta)
+                    histTranslate.Fill(phi,eta, pEvent.energy[index])
                    
                     #reflect
-                    sumetapos = 0
-                    sumetaneg = 0
-                    if(eta >=0):
-                        sumetapos += pEvent.energy[index]
-                    else:
-                        sumetaneg += pEvent.energy[index]
+                    etaTempV.append(eta)
+                    phiTempV.append(phi)
+                    energyTempV.append(pEvent.energy[index])
 
-                    
+                    if(eta > 0):
+                        sumEtaPos += pEvent.energy[index]
+                    elif(eta < 0):
+                        sumEtaNeg += pEvent.energy[index]
 
+                    #do the reflection
+                    #put greater energy in positive eta(quadrants 1&2)
+            for i, a in enumerate(etaTempV):
+                if(sumEtaPos < sumEtaNeg):
+                    a = -1*a
+            #fill the histogram for reflection
+                histReflect.Fill(phiTempV[i], a, energyTempV[i])
+            #fill a temporary histogram with data from one jet
+                histJetTemp.Fill(phiTempV[i],etaTempV[i], energyTempV[i])
+            # if(jEvent.pIndex[i]  
+
+            #write the temporary histogram to a text file
+            
+            output.write(COLLISION_TYPE)
+            for q in range(DIMENSION_JET_IMAGE):
+                for n in range(DIMENSION_JET_IMAGE):
+                    output.write(" %d " % histJetTemp.GetBinContent(q, n))
+            output.write(" %d %d \n" % (j, iEvent))
+
+
+            etaTempV = []
+            phiTempV = []
+            energyTempV = []
+            histJetTemp.Reset()
+
+        iEvent+=1
 
 
     #see others for examples of iterating through
-    canvasbefore.cd()
-    histbefore.Draw("lego")
-    canvasbefore.SaveAs("before.pdf")
+    canvasBefore.cd()
+    histBefore.Draw("lego")
+    canvasBefore.SaveAs("before.pdf")
 
-    canvascentre.cd()
-    histcentre.Draw("lego")
+    canvasCentre.cd()
+    histCentre.Draw("lego")
     #https://root.cern.ch/root/html534/guides/users-guide/Histograms.html
-    canvascentre.SaveAs("centre.pdf")
+    canvasCentre.SaveAs("centre.pdf")
 
-    canvasrotate.cd()
-    histrotate.Draw("lego")
-    canvasrotate.SaveAs("rotate.pdf")
+    canvasRotate.cd()
+    histRotate.Draw("lego")
+    canvasRotate.SaveAs("rotate.pdf")
 
     canvasTranslate.cd()
     histTranslate.Draw("lego")
     canvasTranslate.SaveAs("translate.pdf")
 
+    canvasReflect.cd()
+    histReflect.Draw("lego")
+    canvasReflect.SaveAs("reflect.pdf")
     
 
 if __name__ == "__main__":
