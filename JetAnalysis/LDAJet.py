d8249ff29086755140eb0ab984826169815a26ee
sc2554 <sc2554@yu.yale.edu@spinup-0005ac.yu.yale.edu>
2017-07-18 10:48:00 -0400

    1d hist works

--- JetAnalysis/LDAJet.py
+++ JetAnalysis/LDAJet.py
@@ -6,43 +6,48 @@ from sklearn import metrics as m
 from sklearn.utils import shuffle
 import ROOT 
 import math
+import sys
 
 HIST_BOUND = 3
 
-input1 = raw_input("Enter the first txt file: ")
-input2 = raw_input("Enter the second txt file: ")
-
-histogram = ROOT.TH1F("histogram", "histogram", 1000, -HIST_BOUND, HIST_BOUND)
-histogram.GetXaxis().SetTitle("X_trans[j]");
-histogram.GetYaxis().SetTitle("frequency");
+inputs = []
+datasets = []
+types = []
 canvas = ROOT.TCanvas("canvas", "canvas")
 
 
+nClasses = input("Enter the number of classes among which you would like to discriminate: ")
+
+inputs.append(raw_input("Enter the first txt file: "))
+#inputs.append(raw_input("Enter the second txt file: "))
+
+if(nClasses < 2):
+    print("Error: you must provide at least 2 classes. Aborting.")
+    sys.exit()
+
 # Open first dataset and read into arrays X,Y, Z
-dataset = pandas.read_csv(input1,sep=" ",header=None)
-array = dataset.values
+datasets.append(pandas.read_csv(inputs[0],sep=" ",header=None))
+array = datasets[0].values
 dimension = array[0, 1]
 print("dimension: %d" % math.pow(dimension, 2))
+print("nClasses: %d" % nClasses)
 X = array[: , 2:int(math.pow(dimension, 2) + 2)]  
 Y = array[: , 0]
+types.append(Y[0])
 Z = array[: , int(math.pow(dimension, 2) + 2):int(math.pow(dimension, 2) + 4)]
 
-#print(X)
-#print(Y)
+for i in range (1, nClasses):
+    inputs.append(raw_input("Enter file %d: " % int(i+1)))
 
+    # Open next dataset and add information onto end of arrays X,Y, Z
+    datasets.append(pandas.read_csv(inputs[i],sep=" ",header=None))
+    array = datasets[i].values
+    X = np.concatenate((X,array[: , 2:int(math.pow(dimension, 2) + 2)]))
+    Y = np.concatenate((Y,array[ : , 0]))
+    types.append(array[0][0])
+    Z = np.concatenate((Z,array[: , int(math.pow(dimension, 2) + 2):int(math.pow(dimension, 2) + 4)]))
 
-# Open second dataset and add information onto end of arrays X,Y, Z
-
-dataset2 = pandas.read_csv(input2,sep=" ",header=None)
-array = dataset2.values
-X = np.concatenate((X,array[: , 2:int(math.pow(dimension, 2) + 2)]))
-Y = np.concatenate((Y,array[ : , 0]))
-Z = np.concatenate((Z,array[: , int(math.pow(dimension, 2) + 2):int(math.pow(dimension, 2) + 4)]))
-
-# print(Y)
-# print(X)
-
-# Randonly split the summed dataset into a training and validation set with 80:20 ratio
+# Randomly split the summed dataset into a training and validation set with 80:20 ratio
 validation_size = 0.15
 seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
@@ -73,26 +78,27 @@ for i in range(Y_validation.size):
         print "Got it wrong", predictions[i],Y_validation[i]
 
 #put all print statements after the loop
-print("X_trans[0][0]")
-print(X_trans[0][0])
+# print("X_trans[0][0]")
+# print(X_trans[0][0])
 
-print("X_trans[1][0]")
-print(X_trans[1][0])
+# print("X_trans[1][0]")
+# print(X_trans[1][0])
 
-print("X_trans[2][0]")
-print(X_trans[2][0])
+# print("X_trans[2][0]")
+# print(X_trans[2][0])
 
-#print("X_trans[0][1]")
-#print(X_trans[0][1])
+# #this one gives error
+# #print("X_trans[0][1]")
+# #print(X_trans[0][1])
 
-print("X_trans[0]")
-print(X_trans[0])
+# print("X_trans[0]")
+# print(X_trans[0])
 
-print("X_trans[1]")
-print(X_trans[1])
+# print("X_trans[1]")
+# print(X_trans[1])
 
-print("X_trans[2]")
-print(X_trans[2])
+# print("X_trans[2]")
+# print(X_trans[2])
 
 print("size of X_trans: ")
 print(X_trans.size)
@@ -105,14 +111,74 @@ print(lda.coef_)
 print("coef_ size: ")
 print(lda.coef_.size)
 
+if (nClasses == 2):
 
-for j in range(X_trans.size):
+    histogram1 = ROOT.TH1F("histogram1", "histogram1", 1000, -HIST_BOUND, HIST_BOUND)
+    histogram2 = ROOT.TH1F("histogram2", "histogram2", 1000, -HIST_BOUND, HIST_BOUND)
+    histogram1.GetXaxis().SetTitle("X_trans[j]");
+    histogram1.GetYaxis().SetTitle("frequency");
 
-    histogram.Fill(X_trans[j][0])
+    for j in range(X_trans.size):
 
-canvas.cd()
-histogram.Draw()
-canvas.SaveAs("first.pdf")
+        if(Y_train[j]  == types[0]):
+
+            histogram1.Fill(X_trans[j][0])
+
+        elif(Y_train[j] == types[1]):
+
+            histogram2.Fill(X_trans[j][0])
+
+# h1->Draw();
+# h1->SetLineColor(kRed);
+# h2->Draw("same");
+# h2->SetLineColor(kBlue);
+# c1->Update();
+
+    title = types[0]+ "-" + types[1]
+
+    canvas.cd()
+    histogram1.Draw()
+    histogram1.SetTitle(title)
+    histogram2.Draw("same")
+    histogram2.SetLineColor(2) #red
+    canvas.SaveAs(title + ".pdf")
+
+# if(nClasses == 3):
+#     histogram1 = ROOT.TH2F("histogram1", "histogram1", 1000, -HIST_BOUND, HIST_BOUND, 1000, -HIST_BOUND, HIST_BOUND)
+#     histogram2 = ROOT.TH2F("histogram2", "histogram2", 1000, -HIST_BOUND, HIST_BOUND, 1000, -HIST_BOUND, HIST_BOUND)
+#     histogram3 = ROOT.TH2F("histogram3", "histogram3", 1000, -HIST_BOUND, HIST_BOUND, 1000, -HIST_BOUND, HIST_BOUND)
+
+#     histogram1.GetZaxis().SetTitle("frequency");
+
+#     print(X_trans.size)
+
+#     # for j in range(X_trans.size):
+
+#     for j in range(X_trans.size):
+
+#         # if(Y_train[j]  == types[0]):
+
+#         histogram1.Fill(X_trans[j][0], X_trans[j][1])
+
+#         # elif(Y_train[j] == types[1]):
+
+#             # histogram2.Fill(X_trans[j][0], X_trans[j][1])
+
+#         # elif(Y_train[j] == types[2]):
+
+#             # histogram3.Fill(X_trans[j][0], X_trans[j][1])
+
+#     title = types[0]+ "-" + types[1] + "-" + types[2]
+#     canvas.cd()
+#     histogram1.Draw("lego")
+#     histogram1.SetTitle(title)
+#     histogram2.Draw("same")
+#     histogram2.SetLineColor(2) #red
+#     histogram3.Draw("same")
+#     histogram3.SetLineColor(3) #green
+#     canvas.SaveAs(title + ".pdf")
+
+
 
 print(m.accuracy_score(Y_validation, predictions))
 #print(m.confusion_matrix(Y_validation, predictions))
